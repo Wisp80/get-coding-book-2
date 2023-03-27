3381b6dc1e08c639c16cec3c80681cc11a573b49
Wisp <therearewispseverywhere@gmail.com>
2023-03-27 16:23:38 +0300

    add main menu, add best of setting, add score tables, add gameover screen, add a lot of refactoring

--- chapter-3/lesson-2/js/lesson-2.js
+++ chapter-3/lesson-2/js/lesson-2.js
@@ -6,6 +6,102 @@ let canvasHeight = 800;
 
 /*-------------------------------------------------------------------------------------------------------------*/
 
+let game = {
+    bestOfOption: 3,
+    playersWins: 0,
+    aiWins: 0,
+    matchesCount: 1,
+
+    tickTimeout: null,
+    tickRate: 1000 / 60,
+
+    tick: function () {
+        window.clearTimeout(game.tickTimeout);
+
+        controls.playersControls.playerOneControls.initializeWatchPlayerOneControls();
+        controls.playersControls.playerOneControls.updatePlayerOneControls(playerOne);
+        controls.aiControls.aiOneControls.updateAiOneControls(aiOne, ballOne);
+
+        ballOne.updateBall(ballOne);
+
+        render.draw([playerOne, aiOne], [ballOne]);
+
+        game.tickTimeout = window.setTimeout('game.tick()', game.tickRate);
+    },
+
+    chooseBestOfOption: function (el) {
+        el.classList.add('highlighted-button')
+
+        for (let i = 0; i < document.getElementsByClassName('best-of-mode-container')[0].children.length; i++) {
+            if (document.getElementsByClassName('best-of-mode-container')[0].children[i] !== el) {
+                document.getElementsByClassName('best-of-mode-container')[0].children[i].classList.remove('highlighted-button');
+            };
+        };
+
+        if (el.innerText === 'Best of 1') {
+            game.bestOfOption = 1;
+        } else if (el.innerText === 'Best of 3') {
+            game.bestOfOption = 3;
+        } else {
+            game.bestOfOption = 5;
+        };
+    },
+
+    startGame: function () {
+        game.updateWinsInfoAndScoreText();
+        document.getElementsByClassName('start-screen')[0].style.display = 'none';
+        document.getElementsByClassName('mainframe')[0].style.display = 'flex';
+        ballOne.reset();
+        game.tick();
+    },
+
+    restart: function () {
+        document.getElementsByClassName('gameover-container')[0].style.display = 'none';
+        document.getElementsByClassName('mainframe')[0].style.display = 'flex';
+        ballOne.reset();
+    },
+
+    goToMenu: function () {
+        document.getElementsByClassName('start-screen')[0].style.display = 'flex';
+        document.getElementsByClassName('gameover-container')[0].style.display = 'none';
+    },
+
+    updateScore: function () {
+        ballOne.reset();
+        playerOne.reset();
+        aiOne.reset();
+
+        game.updateWinsInfoAndScoreText();
+
+        game.matchesCount++;
+
+        if (game.aiWins >= (game.bestOfOption / 2) + 0.5 || game.playersWins >= (game.bestOfOption / 2) + 0.5) {
+            ballOne.freeze();
+
+            document.getElementsByClassName('mainframe')[0].style.display = 'none';
+            document.getElementsByClassName('gameover-container')[0].style.display = 'flex';
+
+            game.updateWinsInfoAndScoreText();
+            game.resetScore();
+
+            document.getElementsByClassName('wins-info')[0].innerHTML = 'You ' + game.playersWins + ' : ' + game.aiWins + ' AI';
+        };
+    },
+
+    updateWinsInfoAndScoreText: function () {
+        document.getElementsByClassName('wins-info')[0].innerHTML = 'You ' + game.playersWins + ' : ' + game.aiWins + ' AI';
+        document.getElementsByClassName('score-text')[0].innerHTML = 'You ' + game.playersWins + ' : ' + game.aiWins + ' AI';
+    },
+
+    resetScore: function () {
+        game.aiWins = 0;
+        game.playersWins = 0;
+        game.matchesCount = 1;
+    }
+};
+
+/*-------------------------------------------------------------------------------------------------------------*/
+
 function Paddle(x, y, width, height, speedModifier, currentSpeedModifier) {
     this.x = x;
     this.y = y;
@@ -46,6 +142,11 @@ function Paddle(x, y, width, height, speedModifier, currentSpeedModifier) {
         nextY = nextY + this.height > canvasHeight ? canvasHeight - this.height : nextY;
         this.y = nextY;
     };
+
+    this.reset = function () {
+        this.x = x;
+        this.y = y;
+    }
 };
 
 let defaultPlayerPaddleWidth = 25;
@@ -55,7 +156,7 @@ let defaultPlayerPaddleYPosition = (canvasHeight - defaultPlayerPaddleHeight) / 
 let defaultPlayerPaddleSpeedModifier = 1;
 let currentPlayerPaddleSpeedModifier = 1.5;
 
-let player = new Paddle(
+let playerOne = new Paddle(
     defaultPlayerPaddleXPosition,
     defaultPlayerPaddleYPosition,
     defaultPlayerPaddleWidth,
@@ -71,7 +172,7 @@ let defaultAiPaddleYPosition = (canvasHeight - defaultAiPaddleHeight) / 2;
 let defaultAiPaddleSpeedModifier = 1;
 let currentAiPaddleSpeedModifier = 1.5;
 
-let ai = new Paddle(
+let aiOne = new Paddle(
     defaultAiPaddleXPosition,
     defaultAiPaddleYPosition,
     defaultAiPaddleWidth,
@@ -98,10 +199,10 @@ function Ball(x, y, radius, xSpeed, ySpeed) {
     };
 
     this.reset = function () {
-        this.x = 800;
-        this.y = 400;
-        this.xSpeed = 6;
-        this.ySpeed = 0;
+        this.x = defaultBallOneXPosition;
+        this.y = defaultBallOneYPosition;
+        this.xSpeed = defaultBallOneXSpeed;
+        this.ySpeed = defaultBallOneYSpeed;
     };
 
     this.modifyXSpeedBy = function (modification) {
@@ -121,12 +222,46 @@ function Ball(x, y, radius, xSpeed, ySpeed) {
 
         this.ySpeed += modification;
     };
+
+    this.freeze = function () {
+        this.xSpeed = 0;
+        this.ySpeed = 0;
+    };
+
+    this.updateBall = function (ball) {
+        ball.x += ball.xSpeed;
+        ball.y += ball.ySpeed;
+
+        if (ball.x < 0) {
+            game.aiWins++;
+            game.updateScore();
+        };
+
+        if (ball.x > canvasWidth) {
+            game.playersWins++;
+            game.updateScore();
+        };
+
+        if (ball.y <= 0 || ball.y >= canvasHeight) {
+            ball.reverseY();
+        };
+
+        let collidedWithPlayer = playerOne.hasCollidedWith(ball);
+        let collidedWithAi = aiOne.hasCollidedWith(ball);
+
+        if (collidedWithPlayer || collidedWithAi) {
+            ball.reverseX();
+            ball.modifyXSpeedBy(1);
+            let speedUpValue = collidedWithPlayer ? playerOne.speedModifier : aiOne.speedModifier;
+            ball.modifyYSpeedBy(speedUpValue);
+        };
+    };
 };
 
 let defaultBallOneXPosition = canvasWidth / 2;
 let defaultBallOneYPosition = canvasHeight / 2;
 let defaultBallOneRadius = 3;
-let defaultBallOneXSpeed = 6;
+let defaultBallOneXSpeed = 11;
 let defaultBallOneYSpeed = 0;
 
 let ballOne = new Ball(
@@ -139,59 +274,59 @@ let ballOne = new Ball(
 
 /*-------------------------------------------------------------------------------------------------------------*/
 
-function updateBall(ball) {
-    ball.x += ball.xSpeed;
-    ball.y += ball.ySpeed;
+let controls = {
+    aiControls: {
+        aiOneControls: {
+            updateAiOneControls: function (ai, ball) {
+                let aiMiddle = ai.y + (ai.height / 2);
 
-    if (ball.x < 0 || ball.x > canvasWidth) {
-        ball.reset();
-    };
+                if (aiMiddle < ball.y) {
+                    ai.move(40);
+                };
 
-    if (ball.y <= 0 || ball.y >= canvasHeight) {
-        ball.reverseY();
-    };
+                if (aiMiddle > ball.y) {
+                    ai.move(38);
+                };
+            }
+        },
 
-    let collidedWithPlayer = player.hasCollidedWith(ball);
-    let collidedWithAi = ai.hasCollidedWith(ball);
+        aiTwoControl: {
+            updateAiTwoControls: function () {
 
-    if (collidedWithPlayer || collidedWithAi) {
-        ball.reverseX();
-        ball.modifyXSpeedBy(1);
-        let speedUpValue = collidedWithPlayer ? player.speedModifier : ai.speedModifier;
-        ball.modifyYSpeedBy(speedUpValue);
-    };
-};
-
-function updateAiControls(ai, ball) {
-    var aiMiddle = ai.y + (ai.height / 2);
-
-    if (aiMiddle < ball.y) {
-        ai.move(40);
-    };
-
-    if (aiMiddle > ball.y) {
-        ai.move(38);
-    };
-};
-
-let playerControls = {
-    heldDown: {},
-
-    updatePlayerControls: function (player) {
-        for (var keyCode in this.heldDown) {
-            player.move(keyCode);
-        };
+            }
+        }
     },
 
-    initializeWatchPlayerControls: function () {
-        window.addEventListener('keydown', () => { this.heldDown[event.keyCode] = true; }, false);
-        window.addEventListener('keyup', () => { delete this.heldDown[event.keyCode]; }, false);
+    playersControls: {
+        playerOneControls: {
+            heldDownKeysByPlayerOne: {},
+
+            updatePlayerOneControls: function (player) {
+                for (let keyCode in this.heldDownKeysByPlayerOne) {
+                    player.move(keyCode);
+                };
+            },
+
+            initializeWatchPlayerOneControls: function () {
+                window.addEventListener('keydown', () => { this.heldDownKeysByPlayerOne[event.keyCode] = true; }, false);
+                window.addEventListener('keyup', () => { delete this.heldDownKeysByPlayerOne[event.keyCode]; }, false);
+            }
+        },
+
+        playerTwoControls: {
+
+        }
     }
 };
 
 /*-------------------------------------------------------------------------------------------------------------*/
 
 let render = {
+    renderField: function () {
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    },
+
     renderPaddle: function (paddle) {
         ctx.fillStyle = 'white';
         ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
@@ -204,83 +339,21 @@ let render = {
         ctx.fill();
     },
 
-    renderField: function () {
-        ctx.fillStyle = 'black';
-        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
-    },
-
-    draw: function () {
+    draw: function (paddles, balls) {
         this.renderField();
-        this.renderPaddle(player);
-        this.renderPaddle(ai);
-        this.renderBall(ballOne);
-    }
-};
 
-/*-------------------------------------------------------------------------------------------------------------*/
-
-function tick() {
-    updateBall(ballOne);
-    playerControls.initializeWatchPlayerControls();
-    playerControls.updatePlayerControls(player);
-    updateAiControls(ai, ballOne);
-
-    render.draw();
-
-    window.setTimeout('tick()', 1000 / 60);
-};
-
-/*-------------------------------------------------------------------------------------------------------------*/
-
-let bestOfOption = 3;
-
-function chooseBestOfOption(el) {
-    el.classList.add('highlighted-button')
-
-    for (let i = 0; i < document.getElementsByClassName('best-of-mode-container')[0].children.length; i++) {
-        if (document.getElementsByClassName('best-of-mode-container')[0].children[i] !== el) {
-            document.getElementsByClassName('best-of-mode-container')[0].children[i].classList.remove('highlighted-button');
+        for (let i = 0; i < paddles.length; i++) {
+            if (paddles[i]) {
+                this.renderPaddle(paddles[i]);
+            };
         };
-    };
 
-    if (el.innerText === 'Best of 1') {
-        bestOfOption = 1;
-    } else if (el.innerText === 'Best of 3') {
-        bestOfOption = 3;
-    } else {
-        bestOfOption = 5;
+        for (let i = 0; i < balls.length; i++) {
+            if (balls[i]) {
+                this.renderBall(balls[i]);
+            };
+        };
     }
 };
 
 /*-------------------------------------------------------------------------------------------------------------*/
-
-function startGame() {
-    document.getElementsByClassName('start-screen')[0].style.display = 'none';
-    document.getElementsByClassName('mainframe')[0].style.display = 'flex';
-
-    initiateGameBasedOnBestOfOption();
-};
-
-function initiateGameBasedOnBestOfOption() {
-    if (bestOfOption === 1) {
-        startBestOfOneMode();
-    } else if (bestOfOption === 3) {
-        startBestOfThreeMode();
-    } else if (bestOfOption === 5) {
-        startBestOfFiveMode();
-    };
-};
-
-function startBestOfOneMode() {
-    tick();    
-};
-
-
-
-
-
-
-
-
-
-
