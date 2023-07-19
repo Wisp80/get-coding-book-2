42a72d47e909246fa9cf2d43f15f6e204707213f
Wisp <therearewispseverywhere@gmail.com>
2023-07-19 19:55:56 +0300

    finish chapter 5 lesson 1

--- chapter-5/lesson-1/js/game.js
+++ chapter-5/lesson-1/js/game.js
@@ -1,34 +1,82 @@
 const game = {
     timer: null,
+
     finished: false,
-    controls: {},
+
+    controls: {
+        left: false,
+        right: false,
+        up: false,
+        down: false,
+
+        mapping: {
+            65: 'left',
+            68: 'right',
+            87: 'up',
+            83: 'down'
+        },
+
+        buttonPress: function (keyInfo) {
+            this[this.mapping[keyInfo.keyCode]] = true;
+        },
+
+        buttonRelease: function (keyInfo) {
+            this[this.mapping[keyInfo.keyCode]] = false;
+        },
+
+        connect: function () {
+            window.addEventListener('keydown', function (keyInfo) {
+                game.controls.buttonPress(event);
+            }, false);
+
+            window.addEventListener('keyup', function (keyInfo) {
+                game.controls.buttonRelease(event);
+            }, false);
+        }
+    },
+
     sounds: {
         enabled: true,
 
         jump: function () {
-            this.play('./src/sounds/meow.wav')
+            this.play('./src/sounds/meow.wav');
         },
 
         backgroundMusic: function () {
-            this.play('./src/sounds/background.mp3')
+            this.play('./src/sounds/background.mp3');
         },
 
         play: function (filename) {
             if (this.enabled) {
-                new Audio(filename.play());
+                new Audio(filename).play();
             };
         }
     },
 
     loop: function () {
+        if (this.finished) {
+            return;
+        };
 
+        world.tick();
+        player.tick();
+        world.draw();
+        player.draw();
+
+        this.timer = window.setTimeout('game.loop()', 1000 / 60);
     },
 
     start: function () {
-
+        this.controls.connect();
+        this.sounds.backgroundMusic();
+        this.loop();
     },
 
-    stop: function () {
-
+    stop: function (reason) {
+        this.finished = true;
+        window.clearTimeout(this.timer);
+        alert(reason === 'win' ? 'You won!' : 'You lost!');
     }
 };
+
+game.start();
--- chapter-5/lesson-1/js/player.js
+++ chapter-5/lesson-1/js/player.js
@@ -1,16 +1,36 @@
 const player = {
-    character: new Character(
-        160, 390, 25, 25,
-        new Animation('./src/cat', 5),
-        new Animation('./src/cat/backwards', 5)
-    ),
+    character: new character(160, 390, 25, 25, new animation('./src/cat/cat', 5), new animation('./src/cat/backwards/cat.backwards', 5)),
 
     tick: function () {
+        let currentLocation = world.getPixelType(this.character.leadingEdge(), this.character.y);
 
+        if (currentLocation === 'exit' || currentLocation === 'pit') {
+            let state = currentLocation === 'exit' ? 'win' : 'lose';
+            game.stop(state);
+            return;
+        };
+
+        this.processControls();
+        this.character.tick();
     },
 
     processControls: function () {
+        if (game.controls.right) {
+            this.character.speed = 5;
+        };
 
+        if (game.controls.left) {
+            this.character.speed = -5;
+        };
+
+        if (!game.controls.left && !game.controls.right) {
+            this.character.speed = 0;
+        };
+
+        if (game.controls.up && this.character.standingOnAPlatform()) {
+            this.character.downwardForce = -8;
+            game.sounds.jump();
+        };
     },
 
     draw: function () {
@@ -18,26 +38,176 @@ const player = {
     }
 };
 
-function Character() {
-
-};
-
-function Animation() {
-
-};
-
-function Enemy(x, y) {
-    this.character = new Character(
-        x, y, 25, 25,
-        new Animation('./src/tiger', 5),
-        new Animation('./src/tiger/backwards', 5)
-    ),
+function character(x, y, width, height, runningSprite, reverseSprite) {
+    this.x = x;
+    this.y = y;
+    this.height = height;
+    this.width = width;
+    this.speed = 0;
+    this.downwardForce = 0;
+    this.jumpHeight = 0;
+    this.runningSprite = runningSprite;
+    this.runningSpriteReversed = reverseSprite;
 
     this.tick = function () {
+        this.applyGravity();
+        this.applyMovement();
+    };
 
+    this.applyGravity = function () {
+        if (this.isJumping()) {
+            this.jumpHeight += (this.downwardForce * -1);
+
+            if (this.jumpHeight >= this.height * 6) {
+                this.downwardForce = world.gravity;
+                this.jumpHeight = 0;
+            };
+
+        } else {
+
+            if (this.standingOnAPlatform()) {
+                this.downwardForce = 0;
+            } else {
+                this.downwardForce = world.gravity;
+            };
+        };
+    };
+
+    this.applyMovement = function () {
+        let nextX = this.x + this.speed;
+        let nextY = this.y + this.downwardForce;
+
+        let nextLeadingX = this.leadingEdge() + this.speed;
+        let walkingIntoSurface = world.isSolidSurface(nextLeadingX, this.y);
+
+        if (this.isMoving() && walkingIntoSurface) {
+            nextX = this.x;
+            this.speed = 0;
+        };
+
+        let topLeftIsSolid = world.isSolidSurface(this.leadingEdge(), this.y);
+        let topRightIsSolid = world.isSolidSurface(this.trailingEdge(), this.y);
+
+        if ((topLeftIsSolid || topRightIsSolid) && this.isJumping()) {
+            this.downwardForce = world.gravity;
+            this.jumpHeight = 0;
+        };
+
+        this.x = nextX;
+        this.y = nextY;
+    };
+
+    this.bottom = function () {
+        return this.y + this.height;
+    };
+
+    this.isJumping = function () {
+        return this.downwardForce < 0;
+    };
+
+    this.isFalling = function () {
+        return this.downwardForce > 0;
+    };
+
+    this.isMoving = function () {
+        return this.speed !== 0;
+    };
+
+    this.leadingEdge = function () {
+        return this.speed < 0 ? this.x : this.x + this.width;
+    };
+
+    this.trailingEdge = function () {
+        return this.speed < 0 ? this.x + this.width : this.x;
+    };
+
+    this.standingOnAPlatform = function () {
+        return world.isSolidSurface(this.leadingEdge(), this.bottom() + 1) ||
+            world.isSolidSurface(this.trailingEdge(), this.bottom() + 1);
+    };
+
+    this.draw = function () {
+        if (!this.runningSprite) {
+            return;
+        };
+
+        let drawAtX = this.x - world.distanceTravelled;
+        drawAtX = drawAtX > this.x ? this.x : drawAtX;
+
+        if (world.atLevelEnd()) {
+            drawAtX = (world.width - (world.level.width - world.distanceTravelled - (this.x - world.distanceTravelled)));
+        };
+
+        let sprite = this.speed < 0 ? this.runningSpriteReversed : this.runningSprite;
+
+        if (this.isJumping() || this.isFalling()) {
+            sprite.drawFrame(4, drawAtX, this.y, this.height, this.width);
+        } else if (this.isMoving()) {
+            sprite.draw(world.tickCount, drawAtX, this.y, this.height, this.width);
+        } else {
+            sprite.drawFrame(1, drawAtX, this.y, this.height, this.width);
+        };
+    };
+
+    this.collidesWith = function (other) {
+        if (this.x >= other.x &&
+            this.x <= other.x + other.width &&
+            this.y >= other.y &&
+            this.y <= other.y + other.height) {
+            return true;
+        };
+
+        return false;
+    };
+};
+
+function animation(filename, frameCount) {
+    this.frames = [];
+
+    this.currentFrameId = 1;
+
+    for (let frameId = 1; frameId <= frameCount; frameId++) {
+        let frame = new Image();
+        frame.src = filename + '.' + frameId + '.png';
+        this.frames[frameId] = frame;
+    };
+
+    this.draw = function (tickCount, x, y, height, width) {
+        if (tickCount % 5 === 0) {
+            this.currentFrameId++;
+        };
+
+        this.currentFrameId = this.currentFrameId >= this.frames.length ? 1 : this.currentFrameId;
+        this.drawFrame(this.currentFrameId, x, y, height, width);
+    };
+
+    this.drawFrame = function (frameNumber, x, y, height, width) {
+        ctx.drawImage(this.frames[frameNumber], x, y, width, height);
+    };
+};
+
+function enemy(x, y) {
+    this.character = new character(x, y, 25, 25, new animation('./src/tiger/tiger', 5), new animation('./src/tiger/backwards/tiger.backwards', 5));
+
+    this.tick = function () {
+        let distanceFromPlayer = Math.abs(player.character.x - this.character.x);
+
+        if (distanceFromPlayer <= world.width * 2) {
+            this.character.speed = 2;
+
+            if (player.character.x < this.character.x) {
+                this.character.speed *= -1;
+            };
+
+            if (this.character.collidesWith(player.character)) {
+                game.stop();
+            };
+
+            this.character.tick();
+        };
     };
 
     this.draw = function () {
         this.character.draw();
-    }
+    };
 };
